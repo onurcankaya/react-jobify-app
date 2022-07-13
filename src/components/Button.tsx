4ed1ce63d8beb5b8ffbe416c1fd93f2e77753064
Onur Cankaya <onur.eren.cankaya@gmail.com>
2022-07-13 14:18:33 +0200

    feat: implement Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import styled, { css } from 'styled-components'
+
+const sharedStyles = css`
+  border-radius: var(--borderRadius);
+  border-color: transparent;
+  padding: 0.5rem 1rem;
+  text-transform: uppercase;
+  background: var(--primary-600);
+  color: var(--white);
+  transition: var(--transition);
+  cursor: pointer;
+  font-size: 0.85rem;
+  &:hover {
+    background: var(--primary-500);
+  }
+`
+export const Button = styled.button`
+  ${sharedStyles}
+`
+export const ExternalLinkButton = styled.a`
+  ${sharedStyles}
+  text-decoration: none;
+`
+export const LinkButton = styled(Link)`
+  ${sharedStyles}
+  text-decoration: none;
+`
