25e225b0b62c44fcfc0ad0ff48c80f8af233efc1
Onur Cankaya <onur.eren.cankaya@gmail.com>
2022-07-15 15:01:56 +0200

    refactor: update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,53 @@
 import { Link } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
+type Props = {
+  transparent?: boolean
+}
+
 const sharedStyles = css`
-  border-radius: var(--borderRadius);
-  border-color: transparent;
-  padding: 0.5rem 1rem;
+  display: inline-block;
+  font-size: 1rem;
   letter-spacing: var(--letterSpacing);
-  background: var(--primary-600);
-  color: var(--white);
   transition: var(--transition);
   cursor: pointer;
-  font-size: 1rem;
+`
+
+const transparentButtonStyles = css`
+  ${sharedStyles}
+  color: var(--primary-600);
+  background-color: transparent;
+  border: none;
   &:hover {
-    background: var(--primary-500);
+    color: var(--primary-700);
   }
 `
-export const Button = styled.button`
+
+const buttonStyles = css`
   ${sharedStyles}
+  color: var(--white);
+  background: var(--primary-600);
+  padding: 0.5rem 1rem;
+  border-radius: var(--borderRadius);
+  border-color: transparent;
+  box-shadow: var(--shadow-2);
+  letter-spacing: var(--letterSpacing);
+  transition: var(--transition);
+  text-transform: capitalize;
+  cursor: pointer;
+  &:hover {
+    background: var(--primary-700);
+    box-shadow: var(--shadow-3);
+  }
 `
-export const ExternalLinkButton = styled.a`
-  ${sharedStyles}
+export const Button = styled.button<Props>`
+  ${({ transparent }) => (transparent ? transparentButtonStyles : buttonStyles)}
+`
+export const ExternalLinkButton = styled.a<Props>`
+  ${({ transparent }) => (transparent ? transparentButtonStyles : buttonStyles)}
   text-decoration: none;
 `
-export const LinkButton = styled(Link)`
-  ${sharedStyles}
+export const LinkButton = styled(Link)<Props>`
+  ${({ transparent }) => (transparent ? transparentButtonStyles : buttonStyles)}
   text-decoration: none;
 `
